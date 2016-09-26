7649ed10ae1ab2ab80642ee50de79e5045e4cc3b
Iván González <ivan.gonzalez@me.com>
2016-09-26 11:01:32 +0200

    MOCA SDK Integration

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.innoquant.moca.phonegap/www/MOCA.js",
+        "id": "com.innoquant.moca.phonegap.MOCA",
+        "pluginId": "com.innoquant.moca.phonegap",
+        "clobbers": [
+            "MOCA"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.innoquant.moca.phonegap": "2.2.2"
+}
+// BOTTOM OF METADATA
+});
